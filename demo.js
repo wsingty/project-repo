5b5a81357c41e137d1a55d517d484b6001bb1c69
wsingty <43829663+wsingty@users.noreply.github.com>
2020-08-05 22:54:15 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,5 +1,10 @@
-
 javascript:
+var summary = document.getElementById('summary-val').innerText;
+var component = document.getElementById('components-val').innerText;
+var url1 = `https://getsupport.atlassian.com/issues/?jql=text ~ '${summary}' AND component = '${component}'`;
+var sen = document.getElementById('senTextViewField').value.replace("SEN-", "");
+var url5 = 'https://cassi.internal.atlassian.com/ui/cloud-sites/' + sen;
+
 class obj{
     constructor(btn, btn_id, btn_name){
        this.btn = document.createElement("BUTTON");
@@ -9,14 +14,13 @@ class obj{
         }
     }
 
-    btn1 = new obj('btn1', 'btn_1', 'GSAC');
-    btn2 = new obj('btn2', 'btn_2', 'JAC');
-    btn3 = new obj('btn3', 'btn_3', 'BUG');
-    btn4 = new obj('btn4', 'btn_4', 'FR');
-    btn5 = new obj('btn5', 'btn_5', 'CASSI');
-    
-var summary = document.getElementById('summary-val').innerText;
-var component = document.getElementById('components-val').innerText;
-var url1 = `https://getsupport.atlassian.com/issues/?jql=text ~ '${summary}' AND component = '${component}'`;
-var sen = document.getElementById('senTextViewField').value.replace("SEN-", "");
+    var btn1 = new obj('btn1', 'btn_1', 'GSAC');
+    var btn5 = new obj('btn5', 'btn_5', 'CASSI');
+    var btn1 = new obj('btn1', 'btn_6', 'KB');
+    var btn2 = new obj('btn2', 'btn_2', 'JAC');
+    var btn3 = new obj('btn3', 'btn_3', 'BUG');
+    var btn4 = new obj('btn4', 'btn_4', 'FR');
 
+    
+document.getElementById("btn_1").setAttribute("onclick", "window.open(url1)");
+document.getElementById("btn_5").setAttribute("onclick", "window.open(url5)");
