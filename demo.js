54a93d063d2f45f2bd7fc1f1a29acfd2316e0f0b
wsingty <43829663+wsingty@users.noreply.github.com>
2020-08-06 01:11:59 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -5,8 +5,8 @@ var url1 = `https://getsupport.atlassian.com/issues/?jql=text ~ '${summary}' OR 
 var sen = document.getElementById('senTextViewField').value.replace("SEN-", "");
 var url5 = 'https://cassi.internal.atlassian.com/ui/cloud-sites/' + sen;
 var url6 = 'https://confluence.atlassian.com/search/?query=' + component;
-var url7 = `https://jira.atlassian.com/issues/?jql=text ~ '${summary}' OR text ~ '${component}' AND issuetype="Bug"`;
-var url8 = `https://jira.atlassian.com/issues/?jql=text ~ '${summary}' OR text ~ '${component}' AND issuetype in (Improvement, "New Feature", Suggestion)`;
+var url7 = `https://jira.atlassian.com/issues/?jql=issuetype="Bug" AND text ~ '${summary}' OR text ~ '${component}'`;
+var url8 = `https://jira.atlassian.com/issues/?jql=issuetype in (Improvement, "New Feature", Suggestion) AND text ~ '${summary}' OR text ~ '${component}'`;
 
 class obj{
     constructor(btn, btn_id, btn_name){
