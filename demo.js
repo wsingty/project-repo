1619fda71c3fd299fc62533ec0b25ad31e0bf64d
wsingty <43829663+wsingty@users.noreply.github.com>
2020-08-06 01:22:54 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -7,6 +7,7 @@ var url5 = 'https://cassi.internal.atlassian.com/ui/cloud-sites/' + sen;
 var url6 = 'https://confluence.atlassian.com/search/?query=' + summary;
 var url7 = `https://jira.atlassian.com/issues/?jql=issuetype="Bug" AND text ~ '${summary}' OR text ~ '${component}'`;
 var url8 = `https://jira.atlassian.com/issues/?jql=issuetype in (Improvement, "New Feature", Suggestion) AND text ~ '${summary}' OR text ~ '${component}'`;
+var url9 = `https://community.atlassian.com/t5/forums/searchpage/tab/message?advanced=false&allow_punctuation=false&q=${summary}`;
 
 class obj{
     constructor(btn, btn_id, btn_name){
@@ -22,7 +23,8 @@ class obj{
 
     var btn1 = new obj('btn1', 'btn_1', 'GSAC');
     var btn5 = new obj('btn5', 'btn_5', 'CASSI');
-    var btn1 = new obj('btn1', 'btn_6', 'KB');
+    var btn6 = new obj('btn6', 'btn_6', 'KB');
+    var btn2 = new obj('btn2', 'btn_2', 'COMMUNITY');
     var btn3 = new obj('btn3', 'btn_3', 'BUG');
     var btn4 = new obj('btn4', 'btn_4', 'FR');
 
@@ -32,5 +34,5 @@ document.getElementById("btn_5").setAttribute("onclick", "window.open(url5)");
 document.getElementById("btn_6").setAttribute("onclick", "window.open(url6)");
 document.getElementById("btn_3").setAttribute("onclick", "window.open(url7)");
 document.getElementById("btn_4").setAttribute("onclick", "window.open(url8)");
-
+document.getElementById("btn_2").setAttribute("onclick", "window.open(url9)");
 
